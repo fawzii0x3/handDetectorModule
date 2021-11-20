c7684a2efc74e18d068f36fcedc27bb286874868
fawzii0x3 <mbenchikha5@gmail.com>
2021-11-20 12:42:08 +0100

    final setup of beta module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+name = 'handDetectorModule',
+version = '0.1',
+author = 'fawzii0x3',
+author_email = 'mbenchikha5@gmail.com',
+packages=find_packages(),
+
+)
